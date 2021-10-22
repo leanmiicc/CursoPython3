582322ceccd90b6f1060506cd3ec4c7154a522ac
Lean <lmicciche@stechs.com.ar>
2021-10-22 16:58:15 -0300

    leccion04
    leccion04

---
+++ leccion04/main.py
@@ -0,0 +1,60 @@
+print("----------------------------------------------------")
+print("Leccion 4.18")
+print("---- Operadores en Python ----")
+print("---- Suma ----")
+operandoA = 10
+operandoB = 5
+suma = operandoA + operandoB
+
+print('El resultado de la suma es:', suma)
+print(f'El resultado suma es: {suma}')
+
+print("----------------------------------------------------")
+print("Leccion 4.19")
+print("---- Operadores en Python ----")
+
+
+print("---- Resta ----")
+resta = operandoA - operandoB
+print(f'El resultado resta es: {resta}')
+
+print("---- Multiplicacion ----")
+multiplicacion = operandoA * operandoB
+print(f'El resultado multiplicacion es: {multiplicacion}')
+
+print("---- Division: Resultado coma flotante ----")
+division = operandoA / operandoB
+print(f'El resultado division es: {division}')
+
+print("---- Division: Resultado parte entera ----")
+division = operandoA // operandoB
+print(f'El resultado division entera es (int): {division}')
+
+print("---- Modulo ----")
+modulo = operandoA % operandoB
+print(f'El resultado modulo es: {modulo}')
+
+print("---- Exponente ----")
+exponente = operandoA ** operandoB
+print(f'El resultado exponente es: {exponente}')
+
+print("----------------------------------------------------")
+print("tarea")
+print("---- Calculo de area y perimetro de un rectangulo ----")
+
+alto = int(input("Proporciona el alto: "))
+ancho = int(input("Proporciona el ancho: "))
+area = alto * ancho
+perimetro = (alto + ancho) * 2
+
+print(f'Area: {area}')
+print(f'Perímetro: {perimetro}')
+
+print("----------------------------------------------------")
+print("Leccion 4.20")
+print("---- Operadores de asignacion en Python ----")
+
+
+
+
+
