8a1913284872e79f6d9dab213c159af3cd8ee3f8
leanmiicc <leandro.micciche@gmail.com>
2021-10-26 16:58:44 -0300

    Leccion04
    Leccion04

--- leccion04/main.py
+++ leccion04/main.py
@@ -13,7 +13,6 @@ print("----------------------------------------------------")
 print("Leccion 4.19")
 print("---- Operadores en Python ----")
 
-
 print("---- Resta ----")
 resta = operandoA - operandoB
 print(f'El resultado resta es: {resta}')
@@ -42,19 +41,82 @@ print("----------------------------------------------------")
 print("tarea")
 print("---- Calculo de area y perimetro de un rectangulo ----")
 
-alto = int(input("Proporciona el alto: "))
-ancho = int(input("Proporciona el ancho: "))
-area = alto * ancho
-perimetro = (alto + ancho) * 2
+##alto = int(input("Proporciona el alto: "))
+##ancho = int(input("Proporciona el ancho: "))
+##area = alto * ancho
+##perimetro = (alto + ancho) * 2
 
-print(f'Area: {area}')
-print(f'Perímetro: {perimetro}')
+##print(f'Area: {area}')
+##print(f'Perímetro: {perimetro}')
 
 print("----------------------------------------------------")
 print("Leccion 4.20")
 print("---- Operadores de asignacion en Python ----")
 
+miVariable = 10
+print(miVariable)
 
+# Reasignacion de variable
+miVariable = miVariable + 1
+print(miVariable)
 
+# Incremento con reasignación:
+miVariable += 1
+print(miVariable)
 
+miVariable -= 2
+# miVariable = miVariable - 2
+print(miVariable)
 
+miVariable *= 3
+print(miVariable)
+
+miVariable /= 2
+print(miVariable)
+
+print("----------------------------------------------------")
+print("Leccion 4.21")
+print("---- Operadores de comparación en Python ----")
+
+a = 4
+b = 5
+resultado = a == b
+print(f'Resultado de == A y B es: {resultado}')
+
+resultado = a != b
+print(f'Resultado de != A y B es: {resultado}')
+
+resultado = a > b
+print(f'Resultado de > A y B es: {resultado}')
+
+resultado = a >= b
+print(f'Resultado de >= A y B es: {resultado}')
+
+resultado = a < b
+print(f'Resultado de < A y B es: {resultado}')
+
+resultado = a <= b
+print(f'Resultado de <= A y B es: {resultado}')
+
+print("----------------------------------------------------")
+print("Leccion 4.21")
+print("---- Ejercicio de numero impar o par ----")
+"""
+a = int(input("Ingrese un numero: "))
+if a % 2 == 0:
+    print(f'El numero {a} es par')
+else:
+    print(f'El numero {a} es impar')
+"""
+
+print("----------------------------------------------------")
+print("Leccion 4.22")
+print("---- Ejercicio de Mayor de edad ----")
+
+edadAdulta = 18
+edad = int(input("Ingrese su edad: "))
+
+if edad >= edadAdulta:
+    print('Usted es mayor de edad.')
+else:
+    print('Usted es menor de edad.')
