d08cbf0e9538023ad32ee918f3bd4a8a958495be
leanmiicc <leandro.micciche@gmail.com>
2021-10-27 17:06:58 -0300

    Leccion04
    Leccion04

--- leccion04/main.py
+++ leccion04/main.py
@@ -112,11 +112,54 @@ else:
 print("----------------------------------------------------")
 print("Leccion 4.22")
 print("---- Ejercicio de Mayor de edad ----")
-
+"""
 edadAdulta = 18
 edad = int(input("Ingrese su edad: "))
 
 if edad >= edadAdulta:
-    print('Usted es mayor de edad.')
+    print(f'La persona tiene {edad} años de edad y es adulta.')
 else:
-    print('Usted es menor de edad.')
+    print(f'La persona tiene {edad} años de edad y es menor.')
+"""
+print("----------------------------------------------------")
+print("Leccion 4.23")
+print("---- Operadores lógicos: ----")
+"""
+a = False
+b = False
+resultado = a and b
+print(f'{a} y {b}. utilizando el operador AND: {resultado}')
+
+resultado = a or b
+print(f'{a} y {b}. Utilizando el operador OR: {resultado}')
+
+resultado = not a
+print(f'{a}. Utilizando el operador NOT: {resultado}')
+"""
+print("----------------------------------------------------")
+print("Leccion 4.23")
+print("---- Ejercicio: Valor dentro de Rango ----")
+""""
+inicio = int(input("Ingrese el inicio del Rango: "))
+fin = int(input("Ingrese el fin del Rango: "))
+valor = int(input("Ingrese un valor numerico a verificar dentro del rango: "))
+dentroRango = valor >= inicio and valor <= fin # Expresion de tipo Boolean
+#dentroRango = inicio <= valor <= fin
+
+if dentroRango == True:
+    print(f'El numero {valor}, se encuentra dentro del rango ({inicio},{fin}) informado.')
+else:
+"""
+
+print("----------------------------------------------------")
+print("Leccion 4.24")
+print("---- Ejercicio: Operador Or - Asistir Juego----")
+
+vacaciones = False
+diaDescanso = False
+asistirJuego = vacaciones or diaDescanso
+
+if asistirJuego:
+    print('El padre puede asistir al juego de su hijo.')
+else:
+    print('El padre no puede asistir al juego de su hijo.')
