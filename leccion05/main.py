98e3c4598a3d800ed42607a95aeec1744f121854
leanmiicc <leandro.micciche@gmail.com>
2021-11-11 10:21:02 -0300

    leccion05
    leccion05

--- leccion05/main.py
+++ leccion05/main.py
@@ -29,7 +29,7 @@ else:
 print(f'El número ingresado es: {a} - Se lee como: {numeroTexto}')"""
 
 print("----------------------------------------------------")
-print("Leccion 5.33")
+print("Leccion 5.34")
 print("---- Sintaxis simplificada del IF ----")
 
 # Forma normal:
@@ -42,3 +42,68 @@ else:
 
 # Forma simplificada:
 print(f"La condicion es: {condicion}") if condicion else print("La condicion es Falsa.")
+
+print("----------------------------------------------------")
+print("Leccion 5.35")
+print("---- Calular estacion segun mes del año. ----")
+"""
+mes = int(input("Ingrese el mes del año (1-12): "))
+estacion = None
+
+if mes == 12 or mes == 1 or mes == 2:
+    estacion = 'Verano'
+elif mes == 3 or mes == 4 or mes == 5:
+    estacion = 'Otoño'
+elif mes == 6 or mes == 7 or mes == 8:
+    estacion = 'Invierno'
+elif mes == 9 or mes == 10 or mes == 11:
+    estacion = 'Primavera'
+else:
+    estacion = 'Mes Incorrecto'
+
+print(f"Para el {mes} la estación es: {estacion}")
+
+"""
+
+print("----------------------------------------------------")
+print("Leccion 5.36")
+print("---- Ejercicio: Etapas de vida segun edad ----")
+
+"""
+edad = int(input("Ingrese su edad: "))
+mensaje = None
+
+if 0 <= edad < 10:
+    mensaje = 'La infancia es increible...'
+elif 10 <= edad < 20:
+    mensaje = 'Muchas cambios y mucho estudio...'
+elif 20 <= edad < 30:
+    mensaje = 'Amor y comienza el trabajo'
+else:
+    mensaje = 'Etapa de vida no reconocida.'
+
+print(f"Tu edad es: {edad}, {mensaje}")
+"""
+
+print("----------------------------------------------------")
+print("Leccion 5.37")
+print("---- Ejercicio: Calificaciones ----")
+
+calificacion = int(input("Ingrese la calificación: "))
+imprimirCalificacion = None
+
+if 9 <= calificacion <= 10:
+    imprimirCalificacion = 'Obtuviste una A'
+elif 8 <= calificacion < 9:
+    imprimirCalificacion = 'Obtuviste una B'
+elif 7 <= calificacion < 8:
+    imprimirCalificacion = 'Obtuviste una C'
+elif 6 <= calificacion < 7:
+    imprimirCalificacion = 'Obtuviste una D'
+elif 0 <= calificacion < 6:
+    imprimirCalificacion = 'Obtuviste una F'
+else:
+    imprimirCalificacion = 'Valor Desconocido.'
+
+print(imprimirCalificacion)
+
